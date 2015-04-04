ea173db65427aee33b707604bbd032a72efcc3a6
Johan Larsson <johan_gavle1@hotmail.com>
2015-04-04 14:25:24 +0200

    Initial

---
+++ Gu.Settings.Fsharp/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Gu.Settings.Fsharp
+
+type Class1() = 
+    member this.X = "F#"
---
+++ Gu.Settings.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gu.Settings.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
