b69bce2ce05daecb63b03a1d244d4b0a1d58e338
Johan Larsson <johan_gavle1@hotmail.com>
2015-04-06 17:26:02 +0200

    WIP

--- Gu.Settings/SettingCollection.cs
+++ Gu.Settings/SettingCollection.cs
@@ -3,12 +3,33 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using Annotations;
+    using Reactive;
 
-    public class SettingCollection : ICollection<ISetting>
+    public class SettingCollection : IEnumerable<ISetting>, INotifyPropertyChanged
     {
+        private readonly List<ISetting> _inner = new List<ISetting>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Count
+        {
+            get { return _inner.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool IsDirty { get; set; }
+
         public IEnumerator<ISetting> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _inner.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -18,36 +39,16 @@
 
         public void Add(ISetting item)
         {
-            throw new NotImplementedException();
+            item.ObservePropertyChanged(x => x.IsDirty)
+                .Subscribe(_ => IsDirty = this.Any(x => x.IsDirty));
+            _inner.Add(item);
         }
 
-        public void Clear()
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(ISetting item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(ISetting[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(ISetting item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count
-        {
-            get { throw new NotImplementedException(); }
-        }
-        public bool IsReadOnly
-        {
-            get { throw new NotImplementedException(); }
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
