83b3e6a1534873cb94407f97696162e5622fe586
Johan Larsson <johan_gavle1@hotmail.com>
2015-06-13 23:54:27 +0200

    Amend

--- Gu.Settings.Tests/SimpleSample.cs
+++ Gu.Settings.Tests/SimpleSample.cs
@@ -6,7 +6,7 @@
     public class SimpleSample
     {
         [Test]
-        public void TestName()
+        public void XmlSample()
         {
             var repository = new XmlRepository();
             var setting = repository.ReadOrCreate(() => new DummySerializable());
