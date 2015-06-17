60ec9309a56fb89b2e4247409dd82e35dc461030
Johan Larsson <johan_gavle1@hotmail.com>
2015-06-17 14:20:05 +0200

    Exposed methods for removing from cache. Fixes #13

--- Gu.Settings/Repositories/DirtyTracker.cs
+++ Gu.Settings/Repositories/DirtyTracker.cs
@@ -12,11 +12,13 @@
         private readonly ConcurrentDictionary<string, object> _cache = new ConcurrentDictionary<string, object>(StringComparer.OrdinalIgnoreCase);
         public DirtyTracker(ICloner cloner)
         {
+            Ensure.NotNull(cloner, "cloner");
             _cloner = cloner;
         }
 
         public virtual void TrackOrUpdate<T>(FileInfo file, T item)
         {
+            Ensure.NotNull(file, "file");
             var clone = _cloner.Clone(item);
             _cache.AddOrUpdate(file.FullName, clone, (f, o) => clone);
         }
@@ -28,8 +30,30 @@
             _cache.ChangeKey(oldName.FullName, newName.FullName, owerWrite);
         }
 
+        public void ClearCache()
+        {
+            _cache.Clear();
+        }
+
+        public void RemoveFromCache(FileInfo file)
+        {
+            Ensure.NotNull(file, "file");
+            object temp;
+            _cache.TryRemove(file.FullName, out temp);
+        }
+
+        /// <summary>
+        /// Only checks the cache, does not read from file.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <param name="file"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
         public virtual bool IsDirty<T>(T item, FileInfo file, IEqualityComparer<T> comparer)
         {
+            Ensure.NotNull(file, "file");
+            Ensure.NotNull(comparer, "comparer");
             object clone;
             if (_cache.TryGetValue(file.FullName, out clone))
             {
