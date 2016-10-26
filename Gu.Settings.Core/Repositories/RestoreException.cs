3c987015c0da0f4edfdac084a30c62643a5b1763
JohanLarsson <johan_gavle1@hotmail.com>
2016-10-26 23:27:55 +0200

    Refactor RestoreException + test serialization.

--- Gu.Settings.Core/Repositories/RestoreException.cs
+++ Gu.Settings.Core/Repositories/RestoreException.cs
@@ -4,21 +4,34 @@ namespace Gu.Settings.Core
     using System;
     using System.IO;
     using System.Runtime.Serialization;
+    using System.Security.Permissions;
 
+    /// <summary>
+    /// This is thrown when restor fails.
+    /// The exception that was thrown in restore is the inner excdeption.
+    /// </summary>
+    [Serializable]
     public class RestoreException : IOException
     {
-        public RestoreException(Exception exception, Exception restoreException)
+        public RestoreException(Exception saveException, Exception innerException)
+            : base("Restore failed", innerException)
         {
-            SaveException = exception;
-            Exception = restoreException;
+            SaveException = saveException;
         }
-
-        public Exception Exception { get; }
 
         public Exception SaveException { get; }
 
-        protected RestoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+        protected RestoreException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
+            this.SaveException = (Exception)info.GetValue(nameof(SaveException), typeof(Exception));
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(SaveException), this.SaveException, typeof(Exception));
+            base.GetObjectData(info, context);
         }
     }
 }
