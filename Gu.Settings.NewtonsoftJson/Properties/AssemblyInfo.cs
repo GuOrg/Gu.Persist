c84a2d4ba4e8516c65bee79974a8918ede32f40e
Johan Larsson <johan_gavle1@hotmail.com>
2016-10-26 20:49:20 +0200

    Bump version.

--- Gu.Settings.NewtonsoftJson/Properties/AssemblyInfo.cs
+++ Gu.Settings.NewtonsoftJson/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gu.Settings.Json")]
 [assembly: AssemblyDescription("A repository using Newtonsoft.Json")]
 [assembly: AssemblyConfiguration("")]
@@ -14,25 +11,8 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f01d25a5-6d87-4a06-aa95-a7d8cf565eef")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
