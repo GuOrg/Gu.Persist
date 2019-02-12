9ff62bf14865f075201ee62c420a41bae94a1cc6
JohanLarsson <johan_gavle1@hotmail.com>
2019-02-12 19:38:51 +0100

    Update Core to new csproj.

--- Gu.Persist.Core/AssemblyAttributes.cs
+++ Gu.Persist.Core/AssemblyAttributes.cs
@@ -1,21 +1,5 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Gu.Persist")]
-[assembly: AssemblyDescription("Manages saving and reading files.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("Gu.Persist")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("d254e6a5-a3a4-4a75-8ed1-41812208bca6")]
-[assembly: AssemblyVersion("0.2.0.3")]
-[assembly: AssemblyFileVersion("0.2.0.3")]
 [assembly: InternalsVisibleTo("Gu.Persist.Core.Tests", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Persist.Demo", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Persist.NewtonsoftJson", AllInternalsVisible = true)]
@@ -29,4 +13,3 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Gu.Persist.Git", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Persist.Git.Tests", AllInternalsVisible = true)]
 
-[assembly: NeutralResourcesLanguageAttribute("en")]
