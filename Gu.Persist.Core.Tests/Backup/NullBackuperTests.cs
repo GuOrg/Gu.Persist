3fb9f8561303fda2779e587acd175fbd43d228c6
JohanLarsson <johan_gavle1@hotmail.com>
2019-09-16 13:02:19 +0200

    Simplify tests.

--- Gu.Persist.Core.Tests/Backup/NullBackuperTests.cs
+++ Gu.Persist.Core.Tests/Backup/NullBackuperTests.cs
@@ -8,86 +8,27 @@ namespace Gu.Persist.Core.Tests.Backup
 
     using NUnit.Framework;
 
-    public class NullBackuperTests
+    public static class NullBackuperTests
     {
-        private readonly DirectoryInfo directory;
-        private FileInfo file;
-        private FileInfo backup;
-        private FileInfo backupOneMinuteOld;
-        private FileInfo backupOneHourOld;
-        private FileInfo backupOneDayOld;
-        private FileInfo backupOneMonthOld;
-        private FileInfo backupOneYearOld;
-        private FileInfo[] timestampedBackups;
-        private FileInfo softDelete;
-        private FileInfo otherBackup;
-
-        public NullBackuperTests()
-        {
-            directory = Directories.TempDirectory.CreateSubdirectory("Gu.Persist.Tests")
-                                        .CreateSubdirectory(GetType().FullName);
-            _ = directory.CreateIfNotExists();
-
-            file = directory.CreateFileInfoInDirectory("Meh.cfg");
-            softDelete = file.WithAppendedExtension(FileHelper.SoftDeleteExtension);
-            backup = directory.CreateFileInfoInDirectory("Meh.bak");
-            var settings = new BackupSettings(directory.FullName, BackupSettings.DefaultExtension, BackupSettings.DefaultTimeStampFormat, 3, 3);
-            backupOneMinuteOld = backup.WithTimeStamp(DateTime.Now.AddMinutes(-1), settings);
-            backupOneHourOld = backup.WithTimeStamp(DateTime.Now.AddHours(-1), settings);
-            backupOneDayOld = backup.WithTimeStamp(DateTime.Now.AddDays(-1), settings);
-            backupOneMonthOld = backup.WithTimeStamp(DateTime.Now.AddMonths(-1), settings);
-            backupOneYearOld = backup.WithTimeStamp(DateTime.Now.AddYears(-1), settings);
-
-            otherBackup = directory.CreateFileInfoInDirectory("Other.bak").WithTimeStamp(DateTime.Now.AddHours(1), settings);
-
-            timestampedBackups = new[]
-                                      {
-                                          backupOneMinuteOld,
-                                          backupOneHourOld,
-                                          backupOneDayOld,
-                                          backupOneMonthOld,
-                                          backupOneYearOld,
-                                      };
-        }
+        private static readonly DirectoryInfo Directory = Directories.TempDirectory
+                                                                     .CreateSubdirectory("Gu.Persist.Tests")
+                                                                     .CreateSubdirectory(typeof(NullBackuperTests).FullName);
 
         [SetUp]
-        public virtual void SetUp()
+        public static void SetUp()
         {
-            file.Delete();
-            backup.Delete();
-            foreach (var backup in timestampedBackups)
-            {
-                backup.Delete();
-            }
-
-            otherBackup.Delete();
-            otherBackup.CreateFileOnDisk();
-            softDelete.Delete();
+            Directory.Refresh();
+            Directory.CreateIfNotExists();
         }
 
         [TearDown]
-        public void TearDown()
+        public static void TearDown()
         {
-            file.Delete();
-            backup.Delete();
-
-            foreach (var backup in timestampedBackups)
-            {
-                backup.Delete();
-            }
-
-            otherBackup.Delete();
-            softDelete.Delete();
-        }
-
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            directory.Delete(recursive: true);
+            Directory.DeleteIfExists(true);
         }
 
         [Test]
-        public void BackupWhenNotExists()
+        public static void BackupWhenNotExists()
         {
             var file = CreateFile();
             var backup = CreateBackupFile();
@@ -101,7 +42,7 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void TryRestoreWhenHasSoftDelete()
+        public static void TryRestoreWhenHasSoftDelete()
         {
             var dummy = new DummySerializable(1);
             var file = CreateFile();
@@ -121,7 +62,7 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void TryRestoreWhenHasSoftDeleteAndBackup()
+        public static void TryRestoreWhenHasSoftDeleteAndBackup()
         {
             var dummy = new DummySerializable(1);
             var file = CreateFile();
@@ -145,7 +86,7 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void TryRestoreWhenHasBackupAndOriginal()
+        public static void TryRestoreWhenHasBackupAndOriginal()
         {
             var file = CreateFile();
             var backup = CreateBackupFile();
@@ -167,7 +108,7 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void TryRestoreWhenHasBackup()
+        public static void TryRestoreWhenHasBackup()
         {
             var file = CreateFile();
             var backup = CreateBackupFile();
@@ -188,7 +129,7 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void TryRestoreWhenNoFiles()
+        public static void TryRestoreWhenNoFiles()
         {
             var file = CreateFile();
             var backup = CreateBackupFile();
@@ -205,108 +146,48 @@ namespace Gu.Persist.Core.Tests.Backup
         }
 
         [Test]
-        public void PurgeAll()
+        public static void PurgeAll()
         {
             Assert.Inconclusive("do we want this?");
         }
 
         [Test]
-        public void PurgeWhenNoFiles()
+        public static void AfterSaveWhenNoFiles()
         {
             var file = CreateFile();
-            var backup = CreateBackupFile();
             var softDelete = file.GetSoftDeleteFileFor();
             using (var lockedFile = LockedFile.CreateIfExists(file, x => x.Open(FileMode.Open, FileAccess.Read, FileShare.Delete)))
             {
                 NullBackuper.Default.AfterSave(lockedFile);
             }
 
-            AssertFile.Exists(false, backupOneMinuteOld);
-            AssertFile.Exists(false, backupOneHourOld);
-            AssertFile.Exists(false, backupOneDayOld);
-            AssertFile.Exists(false, backupOneMonthOld);
-            AssertFile.Exists(false, backupOneYearOld);
             AssertFile.Exists(false, softDelete);
         }
 
         [Test]
-        public void PurgeWhenHasSoftDelete()
+        public static void AfterSaveWhenHasSoftDelete()
         {
             var file = CreateFile();
-            var backup = CreateBackupFile();
             var softDelete = file.GetSoftDeleteFileFor();
             file.CreateFileOnDisk();
             softDelete.CreateFileOnDisk();
-            foreach (var timestamped in timestampedBackups)
-            {
-                timestamped.CreateFileOnDisk();
-            }
-
             using (var lockedFile = LockedFile.CreateIfExists(file, x => x.Open(FileMode.Open, FileAccess.Read, FileShare.Delete)))
             {
                 NullBackuper.Default.AfterSave(lockedFile);
             }
 
             AssertFile.Exists(true, file);
-            AssertFile.Exists(true, backupOneMinuteOld);
-            AssertFile.Exists(true, backupOneHourOld);
-            AssertFile.Exists(true, backupOneDayOld);
-            AssertFile.Exists(true, backupOneMonthOld);
-            AssertFile.Exists(true, backupOneYearOld);
             AssertFile.Exists(false, softDelete);
         }
 
         [Test]
-        public void PurgeDeletesSoftDeletesNoTimestamp()
-        {
-            var file = CreateFile();
-            var backup = CreateBackupFile();
-            var softDelete = file.GetSoftDeleteFileFor();
-            file.CreateFileOnDisk();
-            softDelete.CreateFileOnDisk();
-            backup.CreateFileOnDisk();
-            using (var lockedFile = LockedFile.CreateIfExists(file, x => x.Open(FileMode.Open, FileAccess.Read, FileShare.Delete)))
-            {
-                NullBackuper.Default.AfterSave(lockedFile);
-            }
-
-            AssertFile.Exists(true, file);
-            AssertFile.Exists(true, backup);
-            AssertFile.Exists(false, softDelete);
-        }
-
-        [Test]
-        public void PurgeDeletesSoftDeletes()
-        {
-            var file = CreateFile();
-            var backup = CreateBackupFile();
-            var softDelete = file.GetSoftDeleteFileFor();
-            foreach (var timestamped in timestampedBackups)
-            {
-                timestamped.CreateFileOnDisk();
-            }
-
-            softDelete.CreateFileOnDisk();
-            file.CreateFileOnDisk();
-            backup.CreateFileOnDisk();
-            using (var lockedFile = LockedFile.CreateIfExists(file, x => x.Open(FileMode.Open, FileAccess.Read, FileShare.Delete)))
-            {
-                NullBackuper.Default.AfterSave(lockedFile);
-            }
-
-            AssertFile.Exists(true, file);
-            AssertFile.Exists(true, backup);
-            AssertFile.Exists(false, softDelete);
-        }
-
-        [Test]
-        public void Rename()
+        public static void Rename()
         {
             Assert.Inconclusive("Backups must be renamed when original file is renamed");
         }
 
-        private FileInfo CreateFile([CallerMemberName] string name = null) => directory.CreateFileInfoInDirectory(name + ".cfg");
+        private static FileInfo CreateFile([CallerMemberName] string name = null) => Directory.CreateFileInfoInDirectory(name + ".cfg");
 
-        private FileInfo CreateBackupFile([CallerMemberName] string name = null) => directory.CreateFileInfoInDirectory(name + ".bak");
+        private static FileInfo CreateBackupFile([CallerMemberName] string name = null) => Directory.CreateFileInfoInDirectory(name + ".bak");
     }
 }
