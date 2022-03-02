92070bdba38cbf09d7af45dc01c491e1150fa40d
JohanLarsson <johan_gavle1@hotmail.com>
2022-03-02 10:18:25 +0100

    Fix build

--- Gu.Persist.Core/AssemblyAttributes.cs
+++ Gu.Persist.Core/AssemblyAttributes.cs
@@ -13,3 +13,56 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Gu.Persist.SystemXml.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d57fbc884857d2d73c25797b0597b94da17be89fe3e7a2389cb5f647391dbcbc7c4a7544be914332802ce5f2ce4d50f972e8c3ce5576600991dc9ce3190c30d49f9bb3c50951c32201ddf0ff73a0f6f2b737458b545278d075318571a2ad29cac6d8523da1f95ef9eacf612f230883aa1e04ce9b23cbb93846b4c623c2b251a7", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Persist.Git, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d57fbc884857d2d73c25797b0597b94da17be89fe3e7a2389cb5f647391dbcbc7c4a7544be914332802ce5f2ce4d50f972e8c3ce5576600991dc9ce3190c30d49f9bb3c50951c32201ddf0ff73a0f6f2b737458b545278d075318571a2ad29cac6d8523da1f95ef9eacf612f230883aa1e04ce9b23cbb93846b4c623c2b251a7", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Persist.Git.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d57fbc884857d2d73c25797b0597b94da17be89fe3e7a2389cb5f647391dbcbc7c4a7544be914332802ce5f2ce4d50f972e8c3ce5576600991dc9ce3190c30d49f9bb3c50951c32201ddf0ff73a0f6f2b737458b545278d075318571a2ad29cac6d8523da1f95ef9eacf612f230883aa1e04ce9b23cbb93846b4c623c2b251a7", AllInternalsVisible = true)]
+
+#if NET45
+#pragma warning disable SA1402, SA1502, SA1600, SA1649, GU0073
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed class AllowNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed class DisallowNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class MaybeNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class MaybeNullWhenAttribute : Attribute
+    {
+        public MaybeNullWhenAttribute(bool returnValue) => this.ReturnValue = returnValue;
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue) => this.ReturnValue = returnValue;
+
+        public bool ReturnValue { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    internal sealed class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName) => this.ParameterName = parameterName;
+
+        public string ParameterName { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    internal sealed class DoesNotReturnAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute : Attribute
+    {
+        public DoesNotReturnIfAttribute(bool parameterValue) => this.ParameterValue = parameterValue;
+
+        public bool ParameterValue { get; }
+    }
+}
+#endif
